2a0dee4e1cd399f90e53ad08e1ea7cccfbf85ac6
HeQa <heqaml@gmail.com>
2025-06-20 15:20:33 +0200

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import url from 'node:url'
 
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@shared": resolve(_dirname, "./shared"),
+    }
+  },
   plugins: [vue()],
 })
